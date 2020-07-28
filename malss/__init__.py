7344e6c075946fa57dcb19e05da37b3d66ecf46c
uec-rk <k1840001@gmail.com>
2020-07-28 21:19:54 +0900

    Change setup environment.

--- malss/__init__.py
+++ malss/__init__.py
@@ -1,9 +1,4 @@
 # -*- coding: utf-8 -*-
 
-__author__ = 'Ryota KAMOSHIDA'
-__version__ = '2.3.1'
-__license__ = 'MIT License: http://www.opensource.org/licenses/mit-license.php'
-
-
 # high level interface
 from .malss import MALSS
--- setup.py
+++ setup.py
@@ -17,44 +17,44 @@ elif sys.version_info.major == 3 and sys.version_info < (3, 6):
     print('python >= 3.6 is required.')
     sys.exit()
 
-try:
-    import numpy
-    import scipy
-except ImportError as inst:
-    print(inst)
-    sys.exit()
-import sklearn
-import matplotlib
-import pandas
-import jinja2
-
-if LooseVersion(numpy.__version__) < LooseVersion('1.10.2'):
-    raise ImportError('numpy >= 1.10.2 is required')
-
-if LooseVersion(scipy.__version__) < LooseVersion('0.16.1'):
-    raise ImportError('scipy >= 0.16.1 is required')
-
-if LooseVersion(sklearn.__version__) < LooseVersion('0.20'):
-    raise ImportError('sklearn >= 0.20 is required')
-
-if LooseVersion(matplotlib.__version__) < LooseVersion('1.5.1'):
-    raise ImportError('matplotlib >= 1.5.1 is required')
-
-if LooseVersion(pandas.__version__) < LooseVersion('0.14.1'):
-    raise ImportError('pandas >= 0.14.1 is required')
-
-if LooseVersion(jinja2.__version__) < LooseVersion('2.8'):
-    raise ImportError('jinja2 >= 2.8 is required')
+# try:
+#     import numpy
+#     import scipy
+# except ImportError as inst:
+#     print(inst)
+#     sys.exit()
+# import sklearn
+# import matplotlib
+# import pandas
+# import jinja2
+# 
+# if LooseVersion(numpy.__version__) < LooseVersion('1.10.2'):
+#     raise ImportError('numpy >= 1.10.2 is required')
+# 
+# if LooseVersion(scipy.__version__) < LooseVersion('0.16.1'):
+#     raise ImportError('scipy >= 0.16.1 is required')
+# 
+# if LooseVersion(sklearn.__version__) < LooseVersion('0.20'):
+#     raise ImportError('sklearn >= 0.20 is required')
+# 
+# if LooseVersion(matplotlib.__version__) < LooseVersion('1.5.1'):
+#     raise ImportError('matplotlib >= 1.5.1 is required')
+# 
+# if LooseVersion(pandas.__version__) < LooseVersion('0.14.1'):
+#     raise ImportError('pandas >= 0.14.1 is required')
+# 
+# if LooseVersion(jinja2.__version__) < LooseVersion('2.8'):
+#     raise ImportError('jinja2 >= 2.8 is required')
 
 
 PACKAGE = "malss"
 NAME = "malss"
 DESCRIPTION = "MALSS: MAchine Learning Support System"
-AUTHOR = __import__(PACKAGE).__author__
-AUTHOR_EMAIL = "malss@malss.com"
-URL = "https://github.com/canard0328/malss/"
-VERSION = __import__(PACKAGE).__version__
-LICENSE = __import__(PACKAGE).__license__
+AUTHOR = "uec-rk"
+AUTHOR_EMAIL = "uec-rk@malss.com"
+URL = "https://github.com/uec-rk/malss/"
+VERSION = "1.0"
+LICENSE = "MIT"
 
 with open('README.rst') as file:
     long_description = file.read()
